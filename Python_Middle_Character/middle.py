96bedaabdf62825663e62437ee1bb22e1c21e058
Melissa <mystic101@gmail.com>
2021-02-08 10:16:53 +0000

    passed and learned about round in python

--- Python_Middle_Character/middle.py
+++ Python_Middle_Character/middle.py
@@ -6,4 +6,4 @@ def get_middle(s):
     if len(s) % 2 == 0:
         return(s[(round(len(s)/2))-1] + s[round(len(s)/2)])
     else:
-        return(s[round(len(s)/2)])
+        return(s[(int(len(s)/2))])
